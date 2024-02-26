dca563ea08799bce0165dcbd85ce0980c4791873
yanlian <yanlian@iu.edu>
2024-02-26 00:00:01 -0500

    coffee-order

--- src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
+++ src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
@@ -3,13 +3,34 @@ package edu.iu.habahram.coffeeorder.repository;
 import edu.iu.habahram.coffeeorder.model.*;
 import org.springframework.stereotype.Repository;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
 @Repository
 public class OrderRepository {
+
+    private static final String DATABASE_FILE = "db.txt";
+
     public Receipt add(OrderData order) throws Exception {
         Beverage beverage = null;
         switch (order.beverage().toLowerCase()) {
             case "dark roast":
                 beverage = new DarkRoast();
+                break;
+            case "decaf":
+                beverage = new Decaf();
+                break;
+            case "espresso":
+                beverage = new Espresso();
+                break;
+            case "house blend":
+                beverage = new HouseBlend();
                 break;
         }
         if (beverage == null) {
@@ -23,11 +44,35 @@ public class OrderRepository {
                 case "mocha":
                     beverage = new Mocha(beverage);
                     break;
+                case "soy":
+                    beverage = new Soy(beverage);
+                    break;
+                case "whip":
+                    beverage = new Whip(beverage);
+                    break;
                 default:
                     throw new Exception("Condiment type '%s' is not valid".formatted(condiment));
             }
+
         }
-        Receipt receipt = new Receipt(beverage.getDescription(), beverage.cost());
+        int id = generateId();
+        Receipt receipt = new Receipt(beverage.getDescription(), beverage.cost(), id);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(DATABASE_FILE, true))) {
+            writer.write(receipt.id() + ", " + receipt.cost() + ", " + receipt.description());
+            writer.newLine();
+        }
+
         return receipt;
     }
+    private int generateId() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(DATABASE_FILE));
+            return lines.size() + 1;
+        } catch (IOException e) {
+            return 1;
+        }
+    }
+
+
 }
