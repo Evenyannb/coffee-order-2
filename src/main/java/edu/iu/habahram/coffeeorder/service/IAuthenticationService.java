c0e450744e55fa43c5643b88c10962c68bf43273
yanlian <yanlian@iu.edu>
2024-04-29 21:44:25 -0400

    coffee-order

---
+++ src/main/java/edu/iu/habahram/coffeeorder/service/IAuthenticationService.java
@@ -0,0 +1,12 @@
+package edu.iu.habahram.coffeeorder.service;
+
+
+
+import edu.iu.habahram.coffeeorder.model.Customer;
+
+import java.io.IOException;
+
+public interface IAuthenticationService {
+    boolean register(Customer customer) throws IOException;
+    boolean login(String username, String password) throws IOException;
+}
